7c5e0f7044081c943390bb1b7cfa92f7bad1c5b9
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-29 14:35:09 -0300

    create connect to wallet button

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,16 +1,37 @@
+import { useWallet } from "@solana/wallet-adapter-react";
+import dynamic from "next/dynamic";
 import React from "react";
 
 // Constantes
-const TWITTER_HANDLE = "web3dev_";
+const TWITTER_HANDLE = "legeannd";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home = () => {
+    const WalletMultiButtonDynamic = dynamic(
+        async () =>
+            (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+        { ssr: false }
+    );
+
+    const wallet = useWallet();
+
+    const renderNotConnectedContainer = () => (
+        <div>
+            <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji"/>
+
+            <div className="button-container">
+                <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+            </div>
+        </div>
+    );
+
     return (
         <div className="App">
             <div className="container">
                 <div className="header-container">
                     <p className="header">🍭 Candy Drop</p>
                     <p className="sub-text">Máquina de NFTs com cunhagem justa</p>
+                    {wallet.publicKey ? "Hello World" : renderNotConnectedContainer()}
                 </div>
                 <div className="footer-container">
                     <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
